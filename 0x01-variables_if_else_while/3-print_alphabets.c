f2c0b7785fa5a347aedce9437ae2458ee3aa2f76
Your Name <dmmuchai@gmail.com>
2024-06-24 08:36:21 -0700

    Task 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Dennis M. Muchai
+ */
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
