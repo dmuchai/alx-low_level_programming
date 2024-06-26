2143be4fed0638dc01fc90f9e458cdbd96cd4e27
Your Name <dmmuchai@gmail.com>
2024-06-26 14:22:08 -0700

    Task  0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Dennis M. Muchai
+ */
+
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return:return 0
+ */
+int main(void)
+{
+	char school[0] = "_putchar";
+	
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
+}
