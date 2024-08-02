ac97b4bcbbce1d744a98e52a8d96b83d0b50fdd2
Your Name <dmmuchai@gmail.com>
2024-08-01 17:16:28 -0700

    First-The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+ * before - prints a specific string before the main,
+ * function in executed.
+ *
+ * Return: no return.
+ */
+
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
