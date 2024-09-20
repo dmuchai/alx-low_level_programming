c179fc1768ed36c0609cb0ee9e5f391104d0cbe2
Your Name <dmmuchai@gmail.com>
2024-09-20 16:22:50 -0700

    Task 5. Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,8 +11,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int n = 0;
 
-	while (head != NULL) {
-		if (n == index) {
+	while (head != NULL)
+	{
+		if (n == index)
+		{
 			return (head);
 		}
 		head = head->next;
