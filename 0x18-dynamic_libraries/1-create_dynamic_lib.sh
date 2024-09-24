84f29e68b47cc003ed89042da7003e059e0f530b
Your Name <dmmuchai@gmail.com>
2024-09-23 19:47:23 -0700

    Task 1-create_dynamic_lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
-
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-
-rm *.o
